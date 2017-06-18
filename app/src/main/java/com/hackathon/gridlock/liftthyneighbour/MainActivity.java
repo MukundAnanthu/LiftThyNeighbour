7fe31f021d1d24c21c3b0b909d754bb1d47a0c13
Mukund Ananthu <mukund.code@gmail.com>
2017-06-18 17:34:36 +0530

    Login UI Layout ready

--- app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
@@ -1,13 +1,24 @@
 package com.hackathon.gridlock.liftthyneighbour;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        TextView tvNewUser = (TextView) findViewById(R.id.tvNewUser);
+        tvNewUser.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }
