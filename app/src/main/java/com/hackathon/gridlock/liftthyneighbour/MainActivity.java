d98778b9a329e6a20309009fecb3052ee7fdb5d4
Mukund Ananthu <mukund.code@gmail.com>
2017-06-18 18:52:35 +0530

    Redirect from login to admin home, user home correctly

--- app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
@@ -8,7 +8,11 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -36,7 +40,44 @@ public class MainActivity extends Activity {
             });
 
             // Handle sign in
-            
+            final CheckBox checkBoxLogInAsAdmin = (CheckBox) findViewById(R.id.checkBoxAdmin);
+            final EditText editTextUserName = (EditText) findViewById(R.id.etUserName);
+            final EditText editTextPassword = (EditText) findViewById(R.id.etPassword);
+            Button buttonSignIn = (Button) findViewById(R.id.btSignIn);
+
+            final Intent redirectToAdminHomeIntent = new Intent(this, AdminHomeActivity.class);
+            final Intent redirectToUserHomeIntent = new Intent(this, UserHomeActivity.class);
+
+            buttonSignIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean isAdmin = checkBoxLogInAsAdmin.isChecked();
+                    String userName = editTextUserName.getText().toString();
+                    String password = editTextPassword.getText().toString();
+                    if(isAdmin) {
+                        boolean isAdminAuthenticated = authenticateAdmin(userName,password);
+                        if (isAdminAuthenticated) {
+                            startActivity(redirectToAdminHomeIntent);
+                        } else {
+                            CharSequence toastMessage = (CharSequence) getResources().getString(R.string.AUTHENTICATION_FAILED);
+                            Toast authenticationFailedToast = Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG);
+                            authenticationFailedToast.show();
+
+                        }
+                    }
+                    else {
+                        boolean isUserAuthenticated = authenticateUser(userName,password);
+                        if(isUserAuthenticated) {
+                            startActivity(redirectToUserHomeIntent);
+                        }
+                        else {
+                            CharSequence toastMessage = (CharSequence) getResources().getString(R.string.AUTHENTICATION_FAILED);
+                            Toast authenticationFailedToast = Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG);
+                            authenticationFailedToast.show();
+                        }
+                    }
+                }
+            });
 
 
         } else {
@@ -52,15 +93,27 @@ public class MainActivity extends Activity {
         }
     }
 
-
-    private boolean isUserAdmin() {
+    private boolean authenticateAdmin(String userName, String password) {
+        //TODO API hit to authenticate admin and store admin token
         return true;
     }
 
-    private boolean isUserSignedIn() {
+    private boolean authenticateUser(String userName, String password) {
+        //TODO API hit to authenticate user and store user token
+        return true;
+    }
 
+    private boolean isUserAdmin() {
+        SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.PREFERENCE_FILE_NAME), Context.MODE_PRIVATE);
+        String isUserAdminKey = getResources().getString(R.string.KEY_IS_USER_ADMIN);
+        return sharedPreferences.getBoolean(isUserAdminKey, false);
+    }
+
+    private boolean isUserSignedIn() {
         SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.PREFERENCE_FILE_NAME), Context.MODE_PRIVATE);
         String isUserSignedKey = getResources().getString(R.string.KEY_IS_USER_SIGNED);
         return sharedPreferences.getBoolean(isUserSignedKey, false);
     }
+
+
 }
