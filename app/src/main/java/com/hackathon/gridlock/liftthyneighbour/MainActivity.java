f592ad8b1fc79a6e69a73be538f5f95b8d5d5599
Mukund Ananthu <mukund.code@gmail.com>
2017-06-18 17:56:50 +0530

    Application redirects to login page if user not already signed in.

--- app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/MainActivity.java
@@ -1,24 +1,40 @@
 package com.hackathon.gridlock.liftthyneighbour;
 
 import android.app.Activity;
-import android.content.res.Resources;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.util.logging.Logger;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        TextView tvNewUser = (TextView) findViewById(R.id.tvNewUser);
-        tvNewUser.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
 
+        //display login screen if user not logged in
+        if (!isUserSignedIn()) {
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_main);
+            TextView tvNewUser = (TextView) findViewById(R.id.tvNewUser);
+            tvNewUser.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        } else {
+            /*if (isUserAdmin()) {
+
+            } else {
+
+            }*/
+        }
+    }
+
+
+    private boolean isUserSignedIn() {
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.PREFERENCE_FILE_NAME), Context.MODE_PRIVATE);
+        String isUserSignedKey = getResources().getString(R.string.KEY_IS_USER_SIGNED);
+        return sharedPreferences.getBoolean(isUserSignedKey, false);
     }
 }
---
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/util/SharedPreferenceKeys.java
@@ -0,0 +1,11 @@
+package com.hackathon.gridlock.liftthyneighbour.util;
+
+/**
+ *
+ * Contains Keys used for all SharedPreference related data.
+ *
+ * Created by mukund on 6/18/17.
+ */
+
+public class SharedPreferenceKeys {
+}
