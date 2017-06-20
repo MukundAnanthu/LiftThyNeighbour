18a2125693887247eb3c2a74567967f664f7fea9
Mukund Ananthu <mukund.code@gmail.com>
2017-06-20 11:55:08 +0530

    Made admin ops clickable and redirected to TenantList/PendingApprovals activity

--- app/src/main/java/com/hackathon/gridlock/liftthyneighbour/AdminHomeActivity.java
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/AdminHomeActivity.java
@@ -1,7 +1,12 @@
 package com.hackathon.gridlock.liftthyneighbour;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class AdminHomeActivity extends Activity {
 
@@ -10,6 +15,30 @@ public class AdminHomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
-        //display admin operations in a list
+        ListView adminOpsListView = (ListView) findViewById(R.id.lvAdminOps);
+        adminOpsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        //Pending Approvals
+                        switchToPendingApprovalsActivity();
+                        break;
+                    case 1:
+                        //Tenant List
+                        switchToTenantListActivity();
+                }
+            }
+        });
+    }
+
+    private void switchToPendingApprovalsActivity() {
+        Intent i = new Intent(this, PendingApprovals.class);
+        startActivity(i);
+    }
+
+    private void switchToTenantListActivity() {
+        Intent i = new Intent(this, TenantList.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/hackathon/gridlock/liftthyneighbour/TenantList.java
@@ -0,0 +1,13 @@
+package com.hackathon.gridlock.liftthyneighbour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TenantList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_list);
+    }
+}
